61abb9ad3a75c03c9a940385ea170a04450d454e
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-08-17 11:36:37 -0500

    Created a working todo list, will mod to make into a workoing brewery applicaiton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import * as d3 from "d3";
+import ToDoList from "./ToDoList";
+import { set, local } from "d3";
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+function App () {
+    const [ todos, setTodos ] = useState([])
+    const todoNameRef = useRef()
+
+    useEffect (()=> {
+        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+        if (storedTodos) setTodos(storedTodos)
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    }, [todos])
+    
+    function toggleTodo(id) {
+        const newTodos = [...todos]
+        const todo = newTodos.find(todo => todo.id === id)
+        todo.complete = !todo.complete
+        setTodos(newTodos)
+    }
+
+    function handleAddTodo(e) {
+        const name = todoNameRef.current.value
+        if (name === '') return 
+        setTodos(prevTodos => {
+            return [...prevTodos, 
+                { 
+                    id: uuidv4(), 
+                    name: name, 
+                    complete: false
+                }]
+            })
+        todoNameRef.current.value = null
+    }
+
+    function handleClearTodos() {
+        const newTodos = todos.filter(todo => !todo.complete)
+        setTodos(newTodos)
+    }
+
+    return (
+    <>
+        <ToDoList todos={todos} toggleTodo={toggleTodo} />
+        <input ref={todoNameRef} type="text" test/>
+        <button onClick={handleAddTodo}>Add ToDo</button>
+        <button onClick={handleClearTodos}>Clear Completed Todos</button>
+        <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    </>
+    );
 }
+export default App; 
 
-export default App;
